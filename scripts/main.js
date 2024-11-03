d21057852c8e16c7b8261c103f8338b2991e656d
SamSmithSMusic <122060875+SamSmithSMusic@users.noreply.github.com>
2024-11-02 21:47:21 -0600

    contact frontend done

--- scripts/main.js
+++ scripts/main.js
@@ -11,3 +11,27 @@ expand.addEventListener('click',() => {
         expand.innerHTML = '☰';
     }
 });
+
+const mailing = document.querySelector(".mailing");
+let mailElements = document.querySelectorAll(".mailing input");
+const mailBox = document.querySelector("#physical");
+
+window.addEventListener("load", () => {
+    mailBox.checked = false;
+  });
+
+mailBox.addEventListener('change',(event) => {
+    let isChecked = event.target.checked;
+    if(isChecked) {
+        mailing.setAttribute("style","display:block");
+        mailElements.forEach(element => {
+            element.setAttribute("required","true");
+        });
+    }
+    else if(!isChecked){
+        mailing.setAttribute("style","display:none");
+        mailElements.forEach(element => {
+            element.setAttribute("required","false");
+        });
+    }
+})
