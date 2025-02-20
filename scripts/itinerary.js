45a025e00c1125a1b04f5153105b59016bdd96d8
SamSmithSMusic <samsmithsmusic@gmail.com>
2025-02-19 21:57:02 -0700

    mobile view finished

--- scripts/itinerary.js
+++ scripts/itinerary.js
@@ -1,14 +1,24 @@
 let events = document.querySelectorAll('.itinItem');
 
+// events.forEach((event) => {
+//     event.addEventListener('click', (e) => {
+//         let paragraph = event.querySelector("p");
+//         if (paragraph.classList.contains("hidden")) {
+//             paragraph.classList.remove("hidden");
+//             event.querySelector("b").innerText = "▼ ";
+//         } else {
+//             paragraph.classList.add("hidden");
+//             event.querySelector("b").innerText = "► ";
+//         }
+//     })
+// });
+
 events.forEach((event) => {
-    event.addEventListener('click', (e) => {
+    event.addEventListener('click', () => {
         let paragraph = event.querySelector("p");
-        if (paragraph.classList.contains("hidden")) {
-            paragraph.classList.remove("hidden");
-            event.querySelector("b").innerText = "▼ ";
-        } else {
-            paragraph.classList.add("hidden");
-            event.querySelector("b").innerText = "► ";
-        }
-    })
+        let arrow = event.querySelector("b");
+
+        paragraph.classList.toggle("show");
+        arrow.classList.toggle("rotate");
+    });
 });
